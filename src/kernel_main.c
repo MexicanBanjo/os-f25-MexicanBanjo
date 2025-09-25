9feb65d0294bb89f9c263f7c07791e898dbbaca9
Edwin Johnson <148599514+MexicanBanjo@users.noreply.github.com>
2025-09-24 23:18:32 -0500

    Update kernel_main.c

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -1,25 +1,52 @@
 
 #include <stdint.h>
+#include "rprintf.h"
 
-#define MULTIBOOT2_HEADER_MAGIC         0xe85250d6
+#define MEMORY 0xB8000
+#define WIDTH  80
+#define HEIGHT 25
+#define MULTIBOOT2_HEADER_MAGIC        0xe85250d6
 
 const unsigned int multiboot_header[]  __attribute__((section(".multiboot"))) = {MULTIBOOT2_HEADER_MAGIC, 0, 16, -(16+MULTIBOOT2_HEADER_MAGIC), 0, 12};
 
-uint8_t inb (uint16_t _port) {
-    uint8_t rv;
-    __asm__ __volatile__ ("inb %1, %0" : "=a" (rv) : "dN" (_port));
-    return rv;
+static int cursor_row = 0;
+static int cursor_col = 0;
+static const uint8_t vga_color = 0x07;
+
+int putc(int data) { //Deliverable 1.
+    volatile uint16_t *vram = (uint16_t*)MEMORY;
+    if (data == '\n') {
+        cursor_col = 0; 
+        cursor_row++; 
+    } else if (data == '\r') { 
+        cursor_col = 0; 
+    } else {
+        int pos = cursor_row * WIDTH + cursor_col;
+        vram[pos] = (vga_color << 8) | (uint8_t)data;
+        cursor_col++;
+        if (cursor_col >= WIDTH) { 
+            cursor_col = 0; 
+            cursor_row++;
+        }
+    }
+
+    if (cursor_row >= HEIGHT) {
+        for (int r = 1; r < HEIGHT; r++)
+            for (int c = 0; c < WIDTH; c++)
+                vram[(r - 1)* WIDTH + c] = vram[r * WIDTH + c];
+        for (int c = 0; c < WIDTH; c++)
+            vram[(HEIGHT - 1) * WIDTH + c] = (vga_color << 8) | ' ';
+        cursor_row = HEIGHT-1;
+        cursor_col = 0;
+    }
+    return data;
 }
 
 void main() {
-    unsigned short *vram = (unsigned short*)0xb8000; // Base address of video mem
-    const unsigned char color = 7; // gray text on black background
-
-    while(1) {
-        uint8_t status = inb(0x64);
-
-        if(status & 1) {
-            uint8_t scancode = inb(0x60);
-        }
-    }
+    esp_printf(putc, "Kernel initialized.\n");
+    esp_printf(putc, "Current execution level: %d\n", 0); // Prints current execution. Deliverable 2.
+    //for (int i = 0; i < 30; i++) { // THIS IS FOR TESTING SCROLL. Deliverable 3.
+        //esp_printf(putc, "Line %d: This is a test of the terminal scroll.\n", i);
+    //}
+    while(1) { }
 }
